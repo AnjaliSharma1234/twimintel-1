86a74d9aa23d20a4cbebda2b0d9b3ac90b78e0da
Susahant <sushantjha8@gmail.com>
2020-03-24 19:47:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,57 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output,State
+import dash_table
+
+
+
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__ , external_stylesheets=external_stylesheets)
+
+
+app.layout = html.Div([
+    html.H1(children="Twimbit NLP Preprocessor"),
+    html.H1(children="Dashboard"),
+    html.Label('Question' ),
+    dcc.Input(id='q',value=' ',type='text'),
+
+
+    html.Label('Answer' ),
+    dcc.Input(id='a',value=' ',type='text'),
+    html.Label('Content' ),
+
+    dcc.Input(id='c',value=' ',type='text'),
+    
+   html.Button(id='submit-button', n_clicks=0, children='Submit'),
+   html.Div(id='my-q'),
+    			
+    
+
+    
+])
+
+@app.callback(
+    Output(component_id='my-q', component_property='children'),
+    
+    [Input('submit-button', 'n_clicks')],
+    [State(component_id='q', component_property='value'),
+    State(component_id='a', component_property='value'),
+    State(component_id='c', component_property='value')])
+
+def update_output_div(n_click,q,a,c):
+    print(q)
+    return '''
+        The Button has been pressed {} times,
+        Question is "{}",
+        and Answer  is "{}"
+        and Contents  is "{}"
+    '''.format(q, a,c)
+
+
+if __name__ == '__main__':
+    
+    app.run_server(debug=True)
